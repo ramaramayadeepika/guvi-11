48a051221632befefaf4c8bf29d0aa97647cec0d
ramaramayadeepika <41770272+ramaramayadeepika@users.noreply.github.com>
2018-07-27 21:51:44 +0530

    Create three.py

---
+++ three.py
@@ -0,0 +1,9 @@
+num3 = float(input("enterthird number:"))
+if(num1> num2)and(num1 > num3):
+largest =num1.
+elif(num2 > num1) and (num2 >num3):
+larggest =num2.
+else:
+else:
+largest=3
+print("the largest number is",largest)
